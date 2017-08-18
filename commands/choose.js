162e4ffcc8ae71cbb1b23b126e3c8bb50714095a
pvtHenk <jannesdrijkoningen@gmail.com>
2017-08-18 18:49:48 +0200

    choose wisely

---
+++ commands/choose.js
@@ -0,0 +1,19 @@
+const R = require("ramda");
+
+const chooseOption = options => {
+    console.log(options);
+    const items = options.split(',').map(R.trim);
+    console.log(items);
+    return items[Math.floor(Math.random() * items.length)];
+};
+
+module.exports = {
+    name: 'choose',
+    match: /^(choose)\ (.+)/gi,
+    usage: 'choose option1,option2,...',
+    help: 'For those occasions where you can\'t make a simple decision on your own..',
+    run: async({message, matches}) => {
+        console.log('choosing....');
+        message.channel.send(chooseOption(matches[2]));
+    }
+};
